36fed97b8f072ac78cc5a25e90d060d91163a714
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2025-04-18 03:42:40 +0700

    feat(user): get all users, get user, update user, delete user

---
+++ backend/controllers/userController.js
@@ -0,0 +1,73 @@
+const User = require("../models/userModel");
+
+
+
+const getUsers = async (req, res) => {
+    try {
+        const users = await User.find({
+            deleted : false
+        });
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findOne({
+            _id: req.params.id,
+            deleted : false
+        }).select("name email phone address role token")
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const updateIn4 = req.body
+
+       await User.updateOne({
+            _id : id
+        }, {
+            $set : updateIn4
+        })
+        const updatedUser = await User.findOne({ _id: id }).select("-password");
+
+
+        res.status(200).json({
+            message: "Update user successfully.",
+            user: updatedUser
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+const deleteUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        await User.updateOne({
+            _id : id
+        }, {
+            $set : {
+                deleted : true
+            }
+        })
+        res.status(200).json({
+            message: "Delete user successfully."
+        });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+module.exports = {
+    getUsers,
+    getUser,
+    updateUser,
+    deleteUser
+}
