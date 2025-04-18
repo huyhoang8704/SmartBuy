9c6163adcaf1f5d8f8376a7dd64ed978bbad1110
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2025-04-18 15:26:34 +0700

    feat(Product): get Product

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.post('/',controller.createProduct)
 router.get('/',controller.getAllProducts)
+router.get('/:slug',controller.getProductBySlug)
 
 module.exports = router;
